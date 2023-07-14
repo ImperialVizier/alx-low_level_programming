0f75b044a61262434b7bddd0762c60b39ca94f62
Ademola <ademolauthman077@gmail.com>
2023-07-14 14:51:04 +0000

    C printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - prints a string using the printf() function
+*@void: Indicates no parameter
+*
+*Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
