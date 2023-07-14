27195146aa5d7d40acf2ea57548f0d53a864bb45
Ademola <ademolauthman077@gmail.com>
2023-07-14 14:46:26 +0000

    C puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints out a string using puts()
+* @void: Indicates no parameter
+*
+* Return: The return value is 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
