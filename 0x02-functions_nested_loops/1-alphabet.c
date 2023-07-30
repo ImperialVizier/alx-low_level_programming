a77a7dc4bdb56978915749ed0f99eca06c99b0b0
Ademola <ademolauthman077@gmail.com>
2023-07-30 12:29:20 +0100

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_alphabet - prints all alphabets
+*/
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+}
