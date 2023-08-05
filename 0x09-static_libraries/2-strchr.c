04b3b826387378e852c4c1ef63660e2742ae709f
ademola <ademolauthman077@gmail.com>
2023-08-05 11:04:40 +0100

    static library

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strchr - Locates character in a string
+* @s: String to be searched from
+* @c: character to search
+*
+* Return: Pointer to first found
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (c == *s)
+			return (s);
+		s++;
+	}
+	if (c == '\0')
+		return (s);
+	return (NULL);
+}
